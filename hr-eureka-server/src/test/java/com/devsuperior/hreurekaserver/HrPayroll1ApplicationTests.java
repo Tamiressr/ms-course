608ae6282102bacd2a7353218db9f87d80898610
Tamires <tamiressiqueira12gmail.com>
2021-11-10 10:47:20 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/com/devsuperior/hreurekaserver/HrPayroll1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hreurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayroll1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
